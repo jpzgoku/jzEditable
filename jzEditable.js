78369e0909eeed7b9037ad9abd15c45d9cd6d46e
johnzarek <jpzgoku@hotmail.com>
2017-05-03 19:13:32 +0700

    fixed some bugs and made it more efficient

--- jzEditable.js
+++ jzEditable.js
@@ -3,39 +3,48 @@
 	$.fn.jzEditable = function(options) {
 
 		var settings = $.extend({
-			self: null,
 			appHeight: null,
 			hasSaveButton: true
 		}, options)
 
-		var divClicked = function() {
-			$(this).off('click', divClicked);
-			settings.self = this;
-			settings.appHeight = $(this).height();
+		var divClicked = function($thisInstance) {
+			$thisInstance.off('click')
+			settings.appHeight = $thisInstance.height();
 			var editableText = $("<textarea class='edit' style='width: 100%;'/>");
-			editableText.val($(this).html());
+			editableText.val($thisInstance.html());
 			editableText.css('height', settings.appHeight);
-			$(this).html(' ');
-			$(this).append(editableText);
+			$thisInstance.html(' ');
+			$thisInstance.append(editableText);
 			editableText.focus();
 			if (settings.hasSaveButton) {
 				var saveButton = $("<button>Save</button>");
-				$(this).append(saveButton);
-				$(saveButton).on('click', editText);
+				$thisInstance.append(saveButton);
+				$(saveButton).on('click', function() {
+					editText($thisInstance);
+				});
 			} else {
-				editableText.on('blur', editText);
+				editableText.on('blur', function() {
+					editText($thisInstance);
+				});
 			}
 		};
 
-		var editText = function() {
+		var editText = function($thisInstance) {
 			var viewableText = $("<div>");
-			viewableText.html($('.edit').val());
-			$(settings.self).replaceWith(viewableText);
-			$(viewableText).on('click', divClicked);
+			viewableText.html($thisInstance.find('.edit').val());
+			$thisInstance.replaceWith(viewableText);
+			$(viewableText).on('click', function() {
+				divClicked($(this));
+			});
 		};
 
-		this.on('click', divClicked);
-		return this
+		this.each(function() {
+			var $thisInstance = $(this);
+			$thisInstance.on('click', function() {
+				divClicked($thisInstance);
+			});
+		});
+		return this;
 	}
 
 })(jQuery);
